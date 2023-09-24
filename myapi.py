c2a0307a375081c9d009d49f630f1664684761ea
Abhinay Dubey <122308740+abhinay7047@users.noreply.github.com>
2023-09-24 13:30:12 +0530

    Update myapi.py
    Add some comment

--- myapi.py
+++ myapi.py
@@ -1,19 +1,45 @@
 import paralleldots
-# Setting your API key
 
 class API:
-
     def __init__(self):
+        # Initialize the ParallelDots API with your API key
         paralleldots.set_api_key('8qIxghqWrB3cl1R5xmZPVQBIHxTFZFLGH66aEdM8uRk')
 
-    def sentiment_analysis(self,text):
+    def sentiment_analysis(self, text):
+        """
+        Perform sentiment analysis on the given text.
+
+        Args:
+            text (str): The text for sentiment analysis.
+
+        Returns:
+            dict: A dictionary containing sentiment analysis results.
+        """
         response = paralleldots.sentiment(text)
         return response
 
-    def ner(self,text):
-        response1=paralleldots.ner(text)
+    def ner(self, text):
+        """
+        Perform named entity recognition (NER) on the given text.
+
+        Args:
+            text (str): The text for NER analysis.
+
+        Returns:
+            dict: A dictionary containing NER analysis results.
+        """
+        response1 = paralleldots.ner(text)
         return response1
 
-    def emotion_prediction(self,text):
-        response2=paralleldots.emotion(text)
+    def emotion_prediction(self, text):
+        """
+        Predict emotions in the given text.
+
+        Args:
+            text (str): The text for emotion prediction.
+
+        Returns:
+            dict: A dictionary containing emotion prediction results.
+        """
+        response2 = paralleldots.emotion(text)
         return response2
